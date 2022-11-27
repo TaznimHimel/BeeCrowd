73ce489cd8802edacb073036e4af0ee76a504381
Himel <minhuztaznim@gmail.com>
2022-11-27 20:15:57 +0600

    BeeCrowd

--- BEE1009.c
+++ BEE1009.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    char SN;
-    float fs, ts, bonus,TOTAL;
-    scanf("%c", SN);
-    scanf("%f %f", &fs, &ts);
-    bonus = (ts * 15) / 100;
-    printf("TOTAL = R$ %0.2f",TOTAL = fs + bonus);
+    double salary, value, TOTAL;
+    char name;
+    scanf("%s %lf  %lf", &name, &salary, &value);
+    TOTAL = salary + value * 0.15;
+    printf("TOTAL = R$ %0.2f\n", TOTAL);
     return 0;
 }
