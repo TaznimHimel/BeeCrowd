d548ee5d75cfc2700bdff9de7f9ee7335c80150e
Himel <minhuztaznim@gmail.com>
2022-11-30 23:07:21 +0600

    BeeCrowd

---
+++ BEE1011.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define pi 3.14159
+int main()
+{
+    double R, VOLUME;
+    scanf("%lf", &R);
+
+    printf("VOLUME = %.3lf\n", VOLUME = (4.0 / 3) * pi * R * R * R);
+
+    return 0;
+}
