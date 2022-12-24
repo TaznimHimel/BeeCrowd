a2698361f7cbcade1a0f869bf48e4a06847f63be
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2022-12-24 19:28:16 +0600

    BeeCrowd

--- BEE1017.c
+++ BEE1017.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 int main()
 {
-    int t, s;
-    float fuel;
-    scanf("%d%d", &t, & s);
+    float t, s, fuel;
+    scanf("%f%f", &t, &s);
     fuel = (t * s) / 12;
     printf("%0.3f\n", fuel);
     return 0;
